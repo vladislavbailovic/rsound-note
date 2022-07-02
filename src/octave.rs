8078fe33791206735fdb3beb4f5c43043b249724
vladislav <ve@incsub.com>
2022-07-02 05:37:35 +0200

    Implement MIDI note getters

---
+++ src/octave.rs
@@ -0,0 +1,72 @@
+use crate::pitch_class::*;
+use crate::{CONCERT_A_FREQ, CONCERT_A_ORDER};
+
+#[derive(Copy, Clone, Debug)]
+pub enum Octave {
+    Cm1,
+    C0,
+    C1,
+    C2,
+    C3,
+    C4,
+    C5,
+    C6,
+    C7,
+    C8,
+    C9,
+}
+
+impl Octave {
+    pub fn halftones_from_root(&self, pc: &PitchClass) -> i32 {
+        let tone_ord = *pc as i32;
+        let octave_ord = *self as i32;
+
+        (tone_ord + (octave_ord - 1) * 12) - CONCERT_A_ORDER
+    }
+
+    pub fn freq(&self, pc: &PitchClass) -> f64 {
+        (self.raw_freq(pc) * 100.0).round() / 100.0
+    }
+
+    pub fn midi(&self, pc: &PitchClass) -> i32 {
+        let tone_ord = *pc as i32;
+        let octave_ord = *self as i32;
+
+        127.min(tone_ord + octave_ord * 12)
+    }
+
+    pub fn raw_freq(&self, pc: &PitchClass) -> f64 {
+        let distf = self.halftones_from_root(pc) as f64;
+        2.0_f64.powf(distf / 12.0) * CONCERT_A_FREQ as f64
+        // let dist = self.midi(pc) - 69;
+        // 2.0_f64.powf(dist as f64 / 12.0) * CONCERT_A_FREQ as f64
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dist() {
+        assert_eq!(Octave::C4.halftones_from_root(&PitchClass::A), 0);
+        assert_eq!(Octave::C5.halftones_from_root(&PitchClass::C), 3);
+        assert_eq!(Octave::C4.halftones_from_root(&PitchClass::F), -4);
+    }
+
+    #[test]
+    fn test_freq() {
+        assert_eq!(Octave::C4.freq(&PitchClass::A), 440.0);
+        assert_eq!(Octave::C5.freq(&PitchClass::C), 523.25);
+        assert_eq!(Octave::Cm1.freq(&PitchClass::A), 13.75);
+        assert_eq!(Octave::C0.freq(&PitchClass::B), 29.14);
+    }
+
+    #[test]
+    fn test_midi() {
+        assert_eq!(Octave::C4.midi(&PitchClass::A), 69);
+        assert_eq!(Octave::Cm1.midi(&PitchClass::C), 0);
+        assert_eq!(Octave::C0.midi(&PitchClass::A), 21);
+        assert_eq!(Octave::C9.midi(&PitchClass::B), 127);
+    }
+}
---
+++ src/pitch_class.rs
@@ -0,0 +1,72 @@
+use crate::octave::*;
+use crate::{CONCERT_A_FREQ, CONCERT_A_ORDER};
+
+#[derive(Copy, Clone, Debug)]
+pub enum PitchClass {
+    C,
+    Cis,
+    D,
+    Dis,
+    E,
+    F,
+    Fis,
+    G,
+    Gis,
+    A,
+    B,
+    H,
+}
+
+impl PitchClass {
+    fn halftones_from_root(&self, oct: &Octave) -> i32 {
+        let tone_ord = *self as i32;
+        let octave_ord = *oct as i32;
+
+        (tone_ord + (octave_ord - 1) * 12) - CONCERT_A_ORDER
+    }
+
+    pub fn freq(&self, oct: &Octave) -> f64 {
+        (self.raw_freq(oct) * 100.0).round() / 100.0
+    }
+
+    pub fn midi(&self, oct: &Octave) -> i32 {
+        let tone_ord = *self as i32;
+        let octave_ord = *oct as i32;
+
+        127.min(tone_ord + octave_ord * 12)
+    }
+
+    pub fn raw_freq(&self, oct: &Octave) -> f64 {
+        let distf = self.halftones_from_root(oct) as f64;
+
+        2.0_f64.powf(distf / 12.0) * CONCERT_A_FREQ as f64
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dist() {
+        assert_eq!(PitchClass::A.halftones_from_root(&Octave::C4), 0);
+        assert_eq!(PitchClass::C.halftones_from_root(&Octave::C5), 3);
+        assert_eq!(PitchClass::F.halftones_from_root(&Octave::C4), -4);
+    }
+
+    #[test]
+    fn test_freq() {
+        assert_eq!(PitchClass::A.freq(&Octave::C4), 440.0);
+        assert_eq!(PitchClass::C.freq(&Octave::C5), 523.25);
+        assert_eq!(PitchClass::A.freq(&Octave::Cm1), 13.75);
+        assert_eq!(PitchClass::B.freq(&Octave::C0), 29.14);
+    }
+
+    #[test]
+    fn test_midi() {
+        assert_eq!(PitchClass::A.midi(&Octave::C4), 69);
+        assert_eq!(PitchClass::C.midi(&Octave::Cm1), 0);
+        assert_eq!(PitchClass::A.midi(&Octave::C0), 21);
+        assert_eq!(PitchClass::B.midi(&Octave::C9), 127);
+    }
+}
